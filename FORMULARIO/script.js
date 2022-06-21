fefe757a7d560f7b493cbf4e8e77d9d561772e16
Diego Santos <103071006+dieguinhxx@users.noreply.github.com>
2022-06-20 21:20:39 -0300

    Add files via upload

---
+++ FORMULARIO/script.js
@@ -0,0 +1,8 @@
+function show(){
+    nome = document.form1.nome.value;
+    telefone = document.form1.telefone.value;
+    email = document.form1.email.value;
+    dtnasc = document.form1.dtnasc.value;
+     
+    alert("Nome:"+nome+"\n Telefone:"+telefone+"\n Email:"+email+"\n Data de nascimento:"+dtnasc)
+}
